0ccd4285ef2a79201a74a8b6f86fe2b0673d98f3
getulio.souza <getulio.souza@evolutix.com.br>
2023-05-04 22:24:30 -0300

    bugfix: styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <section className='calculator-grid'>
+        <article className='output'>
+          <div className='previous-operand'>123213</div>
+            <div className='current-operand'>5453545</div>
+        </article>
+        <button className='span-two'>AC</button>
+        <button className='red'>DEL</button>
+        <button>/</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className='span-two'>=</button>
+      </section>
+    </>
+  )
 }
 
 export default App;

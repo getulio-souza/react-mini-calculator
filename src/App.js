019d26cd14ad229fe354a63996645dd4d517cd13
getulio.souza <getulio.souza@evolutix.com.br>
2023-05-06 16:41:11 -0300

    feature: printing-number

--- src/App.js
+++ src/App.js
@@ -1,32 +1,69 @@
 import './App.css';
+import { useReducer } from "react";
+import DigitButton from './DigitalButton';
+import OperationButton from './OperationButton';
+
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE:'evaluate'
+}
+
+function reducer(state, {type, payload}) {
+  // eslint-disable-next-line default-case
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      if (payload.digit === "0" && state.currentOperand === "0") {
+        return state;
+      }
+      if (payload.digit === "." && state.currentOperand.includes(".")) {
+        return state;
+      }
+     
+      return {
+        ...state, 
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`
+      }
+    
+    case ACTIONS.CLEAR:
+      return {}
+  }
+}
 
 function App() {
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
+
   return (
     <>
       <section className='calculator-grid'>
         <article className='output'>
-          <div className='previous-operand'>123213</div>
-            <div className='current-operand'>5453545</div>
+          <div className='previous-operand'>{previousOperand} {operation}</div>
+            <div className='current-operand'>{currentOperand}</div>
         </article>
-        <button className='span-two'>AC</button>
-        <button className='red'>DEL</button>
-        <button>/</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>*</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>+</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>-</button>
-        <button>.</button>
-        <button>0</button>
-        <button className='span-two'>=</button>
-      </section>
+        <button
+          className='span-two'
+          onClick={() => dispatch({type: ACTIONS.CLEAR})}
+        >AC</button>
+        <button>DEL</button>
+        <DigitButton digit="/" dispatch={dispatch}/>
+        <DigitButton digit="1" dispatch={dispatch}/>
+        <DigitButton digit="2" dispatch={dispatch}/>
+        <DigitButton digit="3" dispatch={dispatch}/>
+        <OperationButton operation="*" dispatch={dispatch}/>
+        <DigitButton digit="4" dispatch={dispatch}/>
+        <DigitButton digit="5" dispatch={dispatch}/>
+        <DigitButton digit="6" dispatch={dispatch}/>
+        <OperationButton operation="+" dispatch={dispatch}/>
+        <DigitButton digit="7" dispatch={dispatch}/>
+        <DigitButton digit="8" dispatch={dispatch}/>
+        <DigitButton digit="9" dispatch={dispatch}/>
+        <OperationButton operation="-" dispatch={dispatch} />
+        <DigitButton digit="." dispatch={dispatch}/>
+        <DigitButton digit="0" dispatch={dispatch}/>
+        <OperationButton operation="=" className='span-two'/>
+        </section>
     </>
   )
 }

b47c9ab2d2986185f960053d8a72785da699ac80
getulio.souza <getulio.souza@evolutix.com.br>
2023-05-07 21:37:39 -0300

    bugfix: basic-operations

--- src/App.js
+++ src/App.js
@@ -15,21 +15,123 @@ function reducer(state, {type, payload}) {
   // eslint-disable-next-line default-case
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      
+      // clean number field after a calculation is done 
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false
+        }
+      }
+      
       if (payload.digit === "0" && state.currentOperand === "0") {
         return state;
       }
+
       if (payload.digit === "." && state.currentOperand.includes(".")) {
         return state;
       }
      
       return {
-        ...state, 
+        ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`
+      }
+    
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state;
+      }
+
+      //to handle the changing of the operator no screen
+      if (state.currentOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation
+        }
+      }
+    
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        }
+      }
+      
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null
       }
     
     case ACTIONS.CLEAR:
       return {}
+    
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null
+        
+        }
+      }
+      if (state.currentOperand == null) return state
+      if(state.currentOperand.length === 1){
+        return {...state, currentOperand: null}
+      }
+
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1)
+      }
+
+    case ACTIONS.EVALUATE:
+      if (state.operation == null || state.currentOperand == null || state.previousOperand == null) {
+        return state
+      }
+      else {
+        return {
+          ...state,
+          overwrite: true,
+          previousOperand: null,
+          operation: null,
+          currentOperand: evaluate(state)
+        }
+      }
   }
+}
+
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const prev = parseFloat(previousOperand)
+  const current = parseFloat(currentOperand)
+  if (isNaN(prev) || isNaN(current)) return ""
+
+  let computation = ""
+  
+  // eslint-disable-next-line default-case
+  switch (operation) {
+    case "+":
+      computation = prev + current
+      break
+    
+    case "-": 
+      computation = prev - current
+      break
+    
+    case "*":
+      computation = prev * current
+      break
+    
+    case "÷":
+      computation = prev / current
+      break
+  }
+
+  return computation.toString()
 }
 
 function App() {
@@ -46,8 +148,8 @@ function App() {
           className='span-two'
           onClick={() => dispatch({type: ACTIONS.CLEAR})}
         >AC</button>
-        <button>DEL</button>
-        <DigitButton digit="/" dispatch={dispatch}/>
+        <button onClick={()=> dispatch({type: ACTIONS.DELETE_DIGIT})}>DEL</button>
+        <DigitButton digit="÷" dispatch={dispatch}/>
         <DigitButton digit="1" dispatch={dispatch}/>
         <DigitButton digit="2" dispatch={dispatch}/>
         <DigitButton digit="3" dispatch={dispatch}/>
@@ -62,7 +164,7 @@ function App() {
         <OperationButton operation="-" dispatch={dispatch} />
         <DigitButton digit="." dispatch={dispatch}/>
         <DigitButton digit="0" dispatch={dispatch}/>
-        <OperationButton operation="=" className='span-two'/>
+        <button className='span-two' onClick={()=> dispatch ({type: ACTIONS.EVALUATE})}>=</button>
         </section>
     </>
   )
